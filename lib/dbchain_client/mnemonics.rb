020504603d3bc5feb466d5683f599900953d579e
Ethan Zhang <yzhang.wa@gmail.com>
2021-12-09 00:06:53 +0800

    to fix the ruby version to 3.0.2 to refactor mnemonics related code into class Mnemonics

---
+++ lib/dbchain_client/mnemonics.rb
@@ -0,0 +1,28 @@
+require "bitcoin"
+
+module DbchainClient
+  class Mnemonics
+    class << self
+      def generate_mnemonic(strength_bits = 128)
+        Bitcoin::Trezor::Mnemonic.generate(strength_bits)
+      end
+
+      def mnemonic_to_master_key(mnemonic)
+        seed = Bitcoin::Trezor::Mnemonic.to_seed(mnemonic)
+        Bitcoin::ExtKey.generate_master(seed.htb)
+      end
+
+      def master_key_to_cosmos_key_pair(master_key)
+        # m/44'/118'/0'/0/0"
+        key = master_key.derive(2**31 + 44).derive(2**31 + 118).derive(2**31).derive(0 ).derive(0)
+        [key.priv, key.pub]
+      end
+
+      def public_key_to_address(pub_key)
+        hash160 = Bitcoin.hash160(pub_key)
+        words = Bitcoin::Bech32.convert_bits(hash160.htb.unpack("C*"), from_bits: 8, to_bits: 5, pad: true)
+        Bitcoin::Bech32.encode("cosmos", words)
+      end
+    end
+  end
+end
