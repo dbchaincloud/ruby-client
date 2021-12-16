3682c72b244fdb101a0018337cb4d38ade9b48d6
Ethan Zhang <yzhang.wa@gmail.com>
2021-12-16 13:32:34 +0800

    to rename test_transactioni.rb to test_writer.rb

--- test/test_writer.rb
+++ test/test_writer.rb
@@ -1,9 +1,8 @@
 require 'minitest/autorun'
 require_relative '../lib/dbchain_client'
 
-class TransactionTest < Minitest::Test
-  def test_send_transaction
-#    mnemonic = "wood word work wood word work wood word work wood word work"
+class WriterTest < Minitest::Test
+  def test_insert_row
     mnemonic = "draft bridge behind upper room media still wool idle spend sunny diamond"
     master_key = DbchainClient::Mnemonics.mnemonic_to_master_key(mnemonic)
     key_pair = DbchainClient::Mnemonics.master_key_to_cosmos_key_pair(master_key)
